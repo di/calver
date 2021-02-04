c36a61392f138ae0a1c3b69576d91359f5299323
Jamie Bliss <jbliss@purestorage.com>
2021-02-04 15:49:43 -0500

    Pull version from file, if it exists

--- src/calver/integration.py
+++ src/calver/integration.py
@@ -1,13 +1,22 @@
 import datetime
+import pathlib
 
 DEFAULT_FORMAT = "%Y.%m.%d"
+
+VERSION_FILE = '.calver-version'
+
+
+def read_version_file(dist):
+    # FIXME: Can we get the project directory?
+    fn = pathlib.Path.cwd() / VERSION_FILE
+    return fn.read_text()
 
 
 def version(dist, keyword, value):
     if not value:
         return
     elif value is True:
-        generate_version = lambda: datetime.datetime.now().strftime(DEFAULT_FORMAT)
+        generate_version = lambda: datetime.datetime.now().strftime(DEFAULT_FORMAT)  # noqa: E501
     elif isinstance(value, str):
         generate_version = lambda: datetime.datetime.now().strftime(value)
     elif getattr(value, "__call__", None):
@@ -15,4 +24,7 @@ def version(dist, keyword, value):
     else:
         return
 
-    dist.metadata.version = generate_version()
+    try:
+        dist.metadata.version = read_version_file(dist)
+    except FileNotFoundError:
+        dist.metadata.version = generate_version()
