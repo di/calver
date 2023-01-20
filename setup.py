8cb0e9e0d7d88aebd9861d557a92a5ffd32f2c18
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-01-20 16:41:58 +0200

    Support Python 3.11, test 3.12-dev, drop EOL 3.5-3.6 (#11)
    * Bump GitHub Actions
    
    * Update CI config
    
    * Support Python 3.11, test 3.12-dev, drop EOL 3.5-3.6
    
    * Add README formatting

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     keywords="calver",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.5",
+    python_requires=">=3.7",
     entry_points={
         "distutils.setup_keywords": [
             "use_calver = calver.integration:version",
