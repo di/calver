b0aba790ccfd81992a21a89319fdda7af632b0a0
Dustin Ingram <di@users.noreply.github.com>
2025-04-01 14:08:50 +0000

    Switch to pyproject.toml as much as possible

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 import os
 import sys
 
-from setuptools import find_packages, setup
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+from setuptools import setup
 
 
 def calver_version(value):
+    here = os.path.abspath(os.path.dirname(__file__))
     src = os.path.join(here, "src")
 
     sys.path.insert(0, src)
@@ -20,27 +16,5 @@ def calver_version(value):
 
 
 setup(
-    name="calver",
-    description="Setuptools extension for CalVer package versions",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/di/calver",
-    author="Dustin Ingram",
-    author_email="di@python.org",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
-    ],
-    keywords="calver",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    python_requires=">=3.9",
-    entry_points={
-        "distutils.setup_keywords": [
-            "use_calver = calver.integration:version",
-        ],
-    },
     version=calver_version(True),
 )
